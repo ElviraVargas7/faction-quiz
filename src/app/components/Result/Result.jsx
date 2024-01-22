0b47a25f94677b82f87c7d56f2d038f90470d68d
ElviraVargas7 <elviravarber@gmail.com>
2024-01-22 12:29:03 -0600

    home redireciton added

--- src/app/components/Result/Result.jsx
+++ src/app/components/Result/Result.jsx
@@ -1,10 +1,14 @@
 "use client"
+
+import "./Result.scss"
 import React, { useEffect, useState } from "react";
 import { useTranslation } from '@/app/i18n/client'
 import { faction } from "@/app/constants/factions";
+import { useRouter } from 'next/navigation'
 
 const Result = ({ lng, answers }) => {
     const { t } = useTranslation(lng, 'results')
+    const router = useRouter()
 
     const [resultingFaction, setResultingFaction] = useState("")
 
@@ -28,10 +32,25 @@ const Result = ({ lng, answers }) => {
         }
     }
 
+    const handleRedirectHome = () => {
+        router.push(`/${lng}`)
+    }
+
     return (
-        <div>
-            <h1>{t(`results:${resultingFaction}:faction`)}</h1>
-            <p>{t(`results:${resultingFaction}:explanation`)}</p>
+        <div className="result">
+            <div className="result__top-container">
+                <h1 className="result__top-container__faction">{t(`results:${resultingFaction}:faction`)}</h1>
+
+                <div className="result__top-container__divider"></div>
+
+                <p className="result__top-container__explanation">{t(`results:${resultingFaction}:explanation`)}</p>
+            </div>
+            
+            <div className="result__home-container">
+                <div className="result__home-container__button" onClick={handleRedirectHome}>
+                    <p className="result__home-container__button__text">{t("home")}</p>
+                </div>
+            </div>
         </div>
     )
 }
